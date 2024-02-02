1eb9e7d751fe96fb1121536ba2ac8d681fe4404e
yun <dbstjdgur78@naver.com>
2024-02-02 19:53:33 +0900

    feat: s3 이미지 업로드 구현

--- src/main/java/com/jointpurchases/global/advice/CustomExceptionHandler.java
+++ src/main/java/com/jointpurchases/global/advice/CustomExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.jointpurchases.global.advice;
 
 
+import com.jointpurchases.domain.product.exception.ImageFailToUploadException;
+import com.jointpurchases.global.common.ServiceResult;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-
+    @ExceptionHandler(ImageFailToUploadException.class)
+    public ResponseEntity<?> ImageFailToUploadExceptionHandler(ImageFailToUploadException e){
+        return ResponseEntity.status(e.getErrorCode().getStatusCode())
+                .body(ServiceResult.fail(e.getErrorCode()));
+    }
 }
--- src/main/java/com/jointpurchases/global/exception/ErrorCode.java
+++ src/main/java/com/jointpurchases/global/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    NOT_FOUND_EMAIL(HttpStatus.BAD_REQUEST, "등록되지 않은 이메일입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"내부 서버 오류가 발생했습니다."),
+    UPLOAD_MAXIMUM_IMAGE(HttpStatus.BAD_REQUEST, "최대 3장의 이미지만 업로드할 수 있습니다."),
+    FAIL_TO_UPLOAD_FILE(HttpStatus.BAD_REQUEST, "이미지 업로드에 실패했습니다.")
+    ;
 
     private final HttpStatus statusCode;
     private final String message;
