ffa0c1e19fa96f682ac4778a30e91834ef05a584
yun <dbstjdgur78@naver.com>
2024-02-02 20:16:23 +0900

    feat: 카테고리 조회 기능 구현

--- src/main/java/com/jointpurchases/domain/category/service/CategoryService.java
+++ src/main/java/com/jointpurchases/domain/category/service/CategoryService.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public interface CategoryService {
 
+    List<CategoryResponseDto> getCategoryList();
+
     CategoryResponseDto createCategory(CategoryRequestDto requestDto);
 
 }
