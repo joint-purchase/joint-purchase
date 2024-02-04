b7ae8372b92c7d988044fad2ff97d1ce832fbeb9
syk25 <syk2245@gmail.com>
2024-02-04 19:12:31 +0900

    feat: controller, config 추가

---
+++ src/main/java/com/jointpurchases/domain/security/contoller/AdminController.java
@@ -0,0 +1,15 @@
+package com.jointpurchases.domain.security.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/secure/admin")
+public class AdminController {
+
+    @GetMapping
+    public String adminHello() {
+        return "Hello World! This is the admin endpoint!";
+    }
+}
