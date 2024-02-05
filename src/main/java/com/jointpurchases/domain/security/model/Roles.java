85a3052f6aa5f72568f706d8e5225c57ff389b4d
syk25 <syk2245@gmail.com>
2024-02-05 12:39:35 +0900

    refactor : 회원가입 기능 개선

---
+++ src/main/java/com/jointpurchases/domain/security/model/Roles.java
@@ -0,0 +1,5 @@
+package com.jointpurchases.domain.security.model;
+
+public enum Roles {
+    ROLE_SELLER, ROLE_ADMIN, ROLE_BUYER
+}
--- src/main/java/com/jointpurchases/domain/security/service/JoinService.java
+++ src/main/java/com/jointpurchases/domain/security/service/JoinService.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 @AllArgsConstructor
 public class JoinService {
@@ -14,22 +16,26 @@ public class JoinService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    // 이후에 boolean으로 바꾸기
+    // 이후에 boolean 으로 바꾸기
     public void joinProcess(JoinDto joinDto) {
         String username = joinDto.getUsername();
         String password = joinDto.getPassword();
+        String address = joinDto.getAddress();
+        String email = joinDto.getEmail();
+        LocalDate birthday = joinDto.getBirthday();
 
         Boolean exists = userRepository.existsByUsername(username);
         if(exists){
             throw new RuntimeException("존재하는 회원 이름입니다.");
         }
-        //TODO 권한부여 로직 수정 필요
-        UserEntity data = UserEntity.builder()
+
+        userRepository.save( UserEntity.builder()
                 .username(username)
                 .password(bCryptPasswordEncoder.encode(password))
+                .address(address)
+                .email(email)
+                .birthday(birthday)
                 .role("ROLE_ADMIN") // 스프리에서 권한부여
-                .build();
-
-        userRepository.save(data);
+                .build());
     }
 }
