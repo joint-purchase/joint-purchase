a93cdf23f03fda132785e73e726078239c632b6c
syk25 <syk2245@gmail.com>
2024-02-13 18:44:35 +0900

    refactor: secret key 숨김

--- src/main/java/com/jointpurchases/domain/security/config/JwtService.java
+++ src/main/java/com/jointpurchases/domain/security/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "29a11a3c03f50ce72abc0884047fd00b22d1da9be7459956e07b511986a6971c";
+    @Value("${spring.jwt.secret}")
+    private static String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
