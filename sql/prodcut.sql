e77c0f4aec84fb449587df133125851c00d89c28
yun <dbstjdgur78@naver.com>
2024-02-23 18:43:27 +0900

    Revert "feat: jwt 설정 추가"
    This reverts commit fa8e31b7fdbedf78c3b43665e5d36ba5697de1c0.

--- sql/prodcut.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO product (productName, description, price, stockQuantity, likeCount, category_id, user_id)
-VALUES ('바나나', '신선한 바나나입니다.', 1000, 50, 0, 1, 4);
--- sql/prodcutImage.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product_image (image_url, product_id,created_at, modified_at)
-VALUES ('https://project-joint-purchaes-bucket.s3.ap-northeast-2.amazonaws.com/0f156cf8-2979-45da-ad00-9514d1b5aa34.%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7+2023-10-23+154928.png', 1,'2024-02-22 12:00:00', '2024-02-22 12:00:00');
-INSERT INTO product_image (image_url, product_id,created_at, modified_at)
-VALUES ('https://project-joint-purchaes-bucket.s3.ap-northeast-2.amazonaws.com/0f156cf8-2979-45da-ad00-9514d1b5aa34.%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7+2023-10-23+154928.png', 1,'2024-02-22 12:00:00', '2024-02-22 12:00:00');
-INSERT INTO product_image (image_url, product_id,created_at, modified_at)
-VALUES ('https://project-joint-purchaes-bucket.s3.ap-northeast-2.amazonaws.com/0f156cf8-2979-45da-ad00-9514d1b5aa34.%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7+2023-10-23+154928.png', 1,'2024-02-22 12:00:00', '2024-02-22 12:00:00');
